9cdd68f3b336e3f8913a38356ddcb16c2d67fcd4
Sjaaky <sjaaky@users.sourceforge.net>
2011-09-23 23:25:04 +0200

    prepare for e2openplugins

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+pkg = 'Extensions.AntiLogo'
+setup (name = 'enigma2-plugin-extensions-antilogo',
+       version = '0.1',
+       description = 'Prevent logo retention',
+       packages = [pkg],
+       package_dir = {pkg: 'plugin'}
+      )
