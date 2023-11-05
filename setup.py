d452ceaa20081ae85b0969271825e2e574609dfe
GhostofGeeeee <130693485+GhostofGeeeee@users.noreply.github.com>
2023-11-05 17:14:14 +0000

    add german language

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
+import setup_translate
 
 pkg = 'Extensions.AntiLogo'
 setup(name='enigma2-plugin-extensions-antilogo',
        version='0.1',
        description='Prevent logo retention',
        packages=[pkg],
-       package_dir={pkg: 'plugin'}
+       package_dir={pkg: 'plugin'},
+       package_data={pkg: ['locale/*/LC_MESSAGES/*.mo']},
+       cmdclass=setup_translate.cmdclass, # for translation
       )
